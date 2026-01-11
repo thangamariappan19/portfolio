6bf8701ef9976681ea3806cdc6dcfffbd5752304
Thanga Mariappan <thangamariappancse@gmail.com>
2026-01-11 14:04:04 +0530

    upgrade

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -3,12 +3,15 @@ import './Footer.css';
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/thangamariappan19'
-      className='link footer__link'
-    >
-      &copy; {new Date().getFullYear()} Thanga Mariappan Pandian
-    </a>
+    <div className='footer__content'>
+      <a
+        href='https://github.com/thangamariappan19'
+        className='link footer__link'
+      >
+        &copy; {new Date().getFullYear()} Thanga Mariappan Pandian
+      </a>
+      <span className='footer__legal'>Licensed under MIT</span>
+    </div>
   </footer>
 );
 
